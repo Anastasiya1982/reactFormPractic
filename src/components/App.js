9768bb4b35d7b768f67e7d7f7d8aa6cb8b62440b
ACER <mahanasty@mail.ru>
2020-11-18 15:07:33 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default class App extends React.Component {
+
+
+  constructor() {
+    super();
+    this.state={
+      username:'',
+      password:'',
+      repeatPassword:'',
+    }
+  }
+  onChange=(event)=>{
+    this.setState({
+      [event.target.name]:event.target.value,
+    });
+  };
+
+
+  onSubmit=(event)=>{
+    event.preventDefault();
+    console.log(this.username.value,this.password.value);
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div className="form-container card">
+        <form className="form card-body">
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter username"
+              ref={node=>this.username=node}
+              value={this.state.username}
+              name='username'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="text"
+              className="form-control"
+              ref={node=>this.password=node}
+              value={this.state.password}
+              name='password'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Repeat password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter repeat password"
+              ref={node=>this.repeatPassword=node}
+              value={this.state.repeatPassword}
+              name='repeatPassword'
+              onChange={this.onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100"
+          onClick={this.onSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
