539387b7037fe8e2620bd34203d983355b8713a9
ACER <mahanasty@mail.ru>
2020-11-18 16:00:30 +0300

    создаем селект универсальныйЭ

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import countries from "../data/countries";
 
 export default class App extends React.Component {
 
@@ -9,6 +10,7 @@ export default class App extends React.Component {
       username:'',
       password:'',
       repeatPassword:'',
+      country:'1',
     }
   }
   onChange=(event)=>{
@@ -22,9 +24,25 @@ export default class App extends React.Component {
     event.preventDefault();
     console.log(this.username.value,this.password.value);
     console.log(this.state)
+  };
+
+  getOptionsItems=(items)=>{
+    return items.map(item => (
+        <option key={item.id}
+                value={item.id}>
+          {item.name}
+        </option>
+    ))
   }
 
   render() {
+
+    // const getOptionsCountries=countries.map(country => (
+    //       <option key={country.id}
+    //               value={country.id}>
+    //         {country.name}
+    //       </option>
+    //   ))
     return (
       <div className="form-container card">
         <form className="form card-body">
@@ -62,6 +80,17 @@ export default class App extends React.Component {
               name='repeatPassword'
               onChange={this.onChange}
             />
+          </div>
+          <div className="form-group">
+            <label htmlFor="country">Country</label>
+            <select className="form-control"
+                    id="country"
+                    value={this.state.country}
+                    name="country"
+                    onChange={this.onChange}
+            >
+               {this.getOptionsItems(countries)}
+             </select>
           </div>
           <button type="submit" className="btn btn-primary w-100"
           onClick={this.onSubmit}
